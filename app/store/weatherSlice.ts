3d870e5448e3099ea390851c08c2a687ff604ce8
Nikhil Pratap Singh <pratapnikhil106@gmail.com>
2024-08-26 16:23:06 +0530

    Set up Redux for State

---
+++ app/store/weatherSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface WeatherState {
+  location: string;
+  data: any;
+}
+
+const initialState: WeatherState = {
+  location: "",
+  data: null,
+};
+
+const weatherSlice = createSlice({
+  name: "weather",
+  initialState,
+  reducers: {
+    setLocation(state, action: PayloadAction<string>) {
+      state.location = action.payload;
+    },
+    setWeatherData(state, action: PayloadAction<any>) {
+      state.data = action.payload;
+    },
+  },
+});
+
+export const { setLocation, setWeatherData } = weatherSlice.actions;
+export default weatherSlice.reducer;
